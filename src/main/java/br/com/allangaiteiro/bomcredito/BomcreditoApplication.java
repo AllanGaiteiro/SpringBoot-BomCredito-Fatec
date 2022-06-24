8e739d2518cd1b891efe165e18be6436ec9c00b2
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-06-23 22:24:10 -0300

    removido os testes do app

--- src/main/java/br/com/allangaiteiro/bomcredito/BomcreditoApplication.java
+++ src/main/java/br/com/allangaiteiro/bomcredito/BomcreditoApplication.java
@@ -1,18 +1,18 @@
 package br.com.allangaiteiro.bomcredito;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.allangaiteiro.bomcredito.model.Institution;
-import br.com.allangaiteiro.bomcredito.model.User;
-import br.com.allangaiteiro.bomcredito.services.OpportunityService;
-
 @SpringBootApplication
-public class BomcreditoApplication {
+public class BomcreditoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(BomcreditoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//TODO: Utilizar como auxilio em testes...
+	}
 }
