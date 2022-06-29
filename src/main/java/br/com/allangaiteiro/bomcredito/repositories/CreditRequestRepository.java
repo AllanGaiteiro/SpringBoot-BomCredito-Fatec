7c4751c723f69cb937e2e4a8e93263088db310b0
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-06-29 17:51:32 -0300

    query dashboards

--- src/main/java/br/com/allangaiteiro/bomcredito/repositories/CreditRequestRepository.java
+++ src/main/java/br/com/allangaiteiro/bomcredito/repositories/CreditRequestRepository.java
@@ -1,10 +1,14 @@
 package br.com.allangaiteiro.bomcredito.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.allangaiteiro.bomcredito.model.CreditRequest;
+import br.com.allangaiteiro.bomcredito.model.RequestDay;
+import br.com.allangaiteiro.bomcredito.model.RequestMonth;
 
 @Repository
 public interface CreditRequestRepository extends JpaRepository<CreditRequest, Integer> {
@@ -20,6 +24,15 @@ public interface CreditRequestRepository extends JpaRepository<CreditRequest, In
     @Query("SELECT  SUM( CASE WHEN MONTH(created_at) = MONTH(NOW()) AND  DAY(created_at) = DAY(NOW()) THEN 1 ELSE 0 END ) from  CreditRequest")
     long countDAY();
 
-    @Query("SELECT  SUM( CASE WHEN MONTH(created_at) = MONTH(NOW()) AND  DAY(created_at) = DAY(NOW())-1 THEN 1 ELSE 0 END ) from  Opportunity")
+    @Query("SELECT  SUM( CASE WHEN MONTH(created_at) = MONTH(NOW()) AND  DAY(created_at) = DAY(NOW())-1 THEN 1 ELSE 0 END ) from  CreditRequest")
     long countBeforeDAY();
+
+    /* dashboard do mes */
+    @Query("SELECT SUM(1) as total, Day(created_at) AS dia FROM CreditRequest where Month(created_at) = Month(now()) GROUP BY Day(created_at)")
+    List<RequestDay> dashboardMonth();
+
+    /* dashboard do ano */
+    @Query("SELECT SUM(1) as Total, Month(created_at) AS month FROM CreditRequest where year (created_at) = year(now()) GROUP BY Month(created_at)")
+    List<RequestMonth> dashboardYear();
+
 }
